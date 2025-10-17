186c57cb915b1c9cbbbfcd24219aa180edce14b4
Rodrigoh <46634610+rodrigoh111@users.noreply.github.com>
2025-10-17 11:56:36 -0300

    Update 02-instalar-pacotes.sh

--- 02-instalar-pacotes.sh
+++ 02-instalar-pacotes.sh
@@ -10,6 +10,7 @@ PACOTES_ESSENCIAIS=(
     software-properties-common apt-transport-https
     ca-certificates gnupg lsb-release ubuntu-restricted-extras
     build-essential
+    nfs-common
     plocate
     lshw inxi lsof strace hdparm
     python3 python3-pip python3-venv python3-dev
